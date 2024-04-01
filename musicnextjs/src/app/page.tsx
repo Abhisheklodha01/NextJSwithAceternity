bee1c93d6f224dc2db61f38ce7ef3a9e15b30646
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-04-01 21:47:40 +0530

    creating instructors and foter section

--- musicnextjs/src/app/page.tsx
+++ musicnextjs/src/app/page.tsx
@@ -1,5 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonialCard from "@/components/TestimonialCard";
 import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -14,6 +16,8 @@ export default function Home() {
     <WhyChooseUs />
     <MusicSchoolTestimonialCard />
     <UpcomingWebinar />
+    <Instructors />
+    <Footer />
     </main>
     </>
   );
