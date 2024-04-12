e64cd604da66323e8aeb3fc56ad375ee98b7c585
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-04-12 19:10:49 +0530

    changing metadata

--- musicnextjs/src/app/contact/page.tsx
+++ musicnextjs/src/app/contact/page.tsx
@@ -1,4 +1,10 @@
 import Contactpage from "./ContactPage";
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: "Contact: Music School",
+};
 
 function Contact() {
   return <Contactpage />;
--- musicnextjs/src/app/courses/page.tsx
+++ musicnextjs/src/app/courses/page.tsx
@@ -1,4 +1,10 @@
 import Coursespage from './CoursesPage'
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: "Courses: Music School",
+};
 
 function Course() {
   return (
